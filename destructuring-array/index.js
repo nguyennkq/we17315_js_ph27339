66465d6a3eb60afcc5e39747beb0f5558c2274a5
nguyennkq <nguyenthega2k2@gmail.com>
2023-01-10 23:19:27 +0700

    vd-1

--- destructuring-array/index.js
+++ destructuring-array/index.js
@@ -5,4 +5,15 @@ const restaurant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
