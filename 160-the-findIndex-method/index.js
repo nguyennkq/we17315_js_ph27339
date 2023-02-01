5e769857fe9a07f7154f6d0a02b359f0bf784954
nguyennkq <nguyenthega2k2@gmail.com>
2023-02-01 22:39:53 +0700

    update the-findindex-method

--- 160-the-findIndex-method/index.js
+++ 160-the-findIndex-method/index.js
@@ -199,3 +199,13 @@ btnClose.addEventListener("click", function (e) {
 
   inputCloseUsername.value = inputClosePin.value = "";
 });
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const firstWithdrawal = movements.find((mov) => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find((acc) => acc.owner === "Jessica Davis");
+console.log(account);
