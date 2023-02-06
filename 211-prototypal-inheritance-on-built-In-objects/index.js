7f6c86ff5d91fbea2213123a8f12082833780ffe
nguyennkq <nguyenthega2k2@gmail.com>
2023-02-06 13:42:28 +0700

    section14-Prototypal Inheritance on Built-In Obj

---
+++ 211-prototypal-inheritance-on-built-In-objects/index.js
@@ -0,0 +1,37 @@
+"use strict";
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never to this!
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+const jonas = new Person("Jonas", 1991);
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir((x) => x + 1);
